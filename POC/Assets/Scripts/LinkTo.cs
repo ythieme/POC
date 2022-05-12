b37d1240dcddb56fd3719b2ca2fae74a1a254d1f
Yovann <y.thieme@rubika-edu.com>
2022-05-12 22:09:00 +0200

    Doit refaire windows gauche chambre

--- POC/Assets/Scripts/LinkTo.cs
+++ POC/Assets/Scripts/LinkTo.cs
@@ -11,9 +11,19 @@ public class LinkTo : MonoBehaviour
     [SerializeField]
     Transform linkedTo;
 
+    bool resetx = false;
+
     // Update is called once per frame
     void Update()
     {
         transform.position = linkedTo.position + offset;
+    }
+
+    private void ResetXOffset()
+    {
+        if (resetx && offset.x != 0)
+        {
+
+        }
     }
 }
